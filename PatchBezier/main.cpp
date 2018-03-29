060fe98962d49297d5ec3aa77b14cccd8d29325d
Michael Chang <changmichael23@gmail.com>
2018-03-30 01:53:26 +0200

    fixed even more rendering

--- PatchBezier/main.cpp
+++ PatchBezier/main.cpp
@@ -850,7 +850,6 @@ void animate()
 
 		if(initialized2)
 			glDrawElements(GL_QUADS, (sqrt(gridPoints3D.size()) - 1)*(sqrt(gridPoints3D.size()) - 1) * 4, GL_UNSIGNED_SHORT, nullptr);
-
 		//(sqrt(gridPoints3D.size()) - 1)*(sqrt(gridPoints3D.size()) - 1)*4
 		//----------------
 		glDisableVertexAttribArray(position_location);
